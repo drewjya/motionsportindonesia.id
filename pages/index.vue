6f9baccb49251bc2f685b6d1806970a922d53fb5
drewjyaa <andre@sahamrakyat.com>
2023-12-14 16:39:46 +0700

    Update image styling in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,11 @@ useHead({
   <div
     class="w-full h-screen flex justify-center items-center bg-black flex-col text-white"
   >
-    <img src="~/assets/logo_motion.png" alt="" class="h-[60vh]" />
+    <img
+      src="~/assets/logo_motion.png"
+      alt=""
+      class="h-[60vh] object-contain"
+    />
     <div class="text-center">
       <div class="text-4xl font-bold">Motion Sport Indonesia</div>
       <div class="italic text-3xl">Coming Soon</div>
